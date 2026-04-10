d9934cae00ad3433097f3becd183e734a2dce772
Farhan Ansari <farhanraza2239@gmail.com>
2026-04-10 19:14:12 +0530

    Initial frontend setup with React Vite and Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      Tailwind Working
+    </div>
+  );
+}
+
+export default App;
